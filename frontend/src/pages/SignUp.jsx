44aaf4c8ac30f20768df60d6198b23cf8f2a2587
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-03-27 04:23:36 +0500

    SignUp

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,15 +2,36 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
   return (
     <div className="flex justify-center h-[100vh] bg-blue-300 text-center">
       <div className="flex w-full justify-center items-center">
         <div className="w-[30%] h-[500px] bg-white flex flex-col px-4 gap-4 justify-center">
-          <Input type="email" placeholder="Enter Your Email" />
-          <Input type="password" placeholder="Enter Your Password" />
-          <Button>Create Account</Button>
+          <form className="flex flex-col text-center justify-center w-full gap-5">
+            <input
+              className="border-slate-200 border-2 h-12 p-3 rounded"
+              type="email"
+              placeholder="Email Address"
+            />
+            <input
+              className="border-slate-200 border-2 h-12 p-3 rounded"
+              type="text"
+              placeholder="Username"
+            />
+            <input
+              className="border-slate-200 border-2 h-12 p-3 rounded"
+              type="password"
+              placeholder="Password"
+            />
+            <button
+              className="bg-black border text-white p-3 rounded hover:bg-white hover:text-black hover:border hover:border-black"
+              type="submit"
+            >
+              Create Your Account
+            </button>
+          </form>
           <p>
             Already have an account?{" "}
             <Link className="font-bold" to={"/"}>
