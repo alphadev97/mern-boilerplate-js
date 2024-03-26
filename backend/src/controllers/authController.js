ae039ebfc2becffe536bba82c819c7ef3e4f5f8d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-03-27 02:53:31 +0500

    authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,20 +1,20 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/errorHandler";
+import emailVerify from "../services/emailVerify.js";
 
-export const signUp = (req, res, next) => {
+export const signUp = async (req, res, next) => {
   try {
     const { email, username, password } = req.body;
-
-    if (!email) {
-      return errorHandler(404, "Email not found!");
-    }
-    if (!username) {
-      return errorHandler(404, "Username not found!");
-    }
-
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
+
+    await newUser.save();
+
+    const user = await User.findOne(newUser);
+
+    emailVerify(user);
+
+    res.status(201).json({ message: "Verification Email Sent!" });
   } catch (error) {
     next(error);
   }
