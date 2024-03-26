57015a508a562a91ff040d29f6bb617821a30dca
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-03-27 01:41:14 +0500

    verifyUser

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,6 +2,5 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = async (id, role, secret) => {
 	const token = jwt.sign({ id, role }, secret, { expiresIn: '1d' });
-
 	return token;
 };
