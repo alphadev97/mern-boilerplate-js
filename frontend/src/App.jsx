6c4577dbbd4410e299b6115dd330b0a29e25cc8d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-03-28 00:38:15 +0500

    Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" Component={SignIn} />
         <Route path="/sign-up" Component={SignUp} />
+        <Route path="/home" Component={Home} />
       </Routes>
     </Router>
   );
